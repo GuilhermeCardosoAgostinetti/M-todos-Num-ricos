4a03bb8485b11f545e501f3465e747f3d92bec7b
guilherme.agostinetti <guilherme.c.27@hotmail.com>
2023-09-29 10:05:09 -0300

    a

--- Optimizacao/Modelo_prob_mistura/prob_mistura.py
+++ Optimizacao/Modelo_prob_mistura/prob_mistura.py
@@ -3,11 +3,10 @@ import xlrd
 import numpy as np
 import pandas as pd
 
-
+fo = 0
 ##restricoes = pd.read_csv('//funcaoobjetiva.csv')
 arq = xlrd.open_workbook('C:\\Users\\Guilherme Cardoso A\\Documents\\GitHub\\Metodos_Numericos\\dados.xlsx')
 plan = arq.sheet_by_name('instancias01')
-func_objetiva = 0
 
 funcao_objetiva = plan.col_values(0)
 print(len(funcao_objetiva))
@@ -18,15 +17,16 @@ model = pulp.LpProblem('Exemplo_01', sense = pulp.LpMaximize)
 model
 x = pulp.LpVariable.dicts(indexs=n_funcao_objetiva, cat = pulp.LpContinuous, lowBound=0, name='x')
 print(x[1])
-model.addConstraint(2*x[1] + 4*x[2] <= 10, name ='restricao_1')
+teste = 2*x[1] + 4*x[2] <= 10
+model.addConstraint(teste, name ='restricao_1')
 model.addConstraint(6*x[1] + x[2] <= 20, name ='restricao_2')
 model.addConstraint(x[1] - x[2]  <= 30, name ='restricao_3')
 
-for i in n_funcao_objetiva:
 
-    func_objetiva = func_objetiva+x[i]
+for i in range(1,3):
+    fo = fo + funcao_objetiva[i-1]*x[i]
 
-model.setObjective(3*x[1] + 5*x[2])
+model.setObjective(fo)
 
 
 model.solve()
