9092adc8284e68842cd4ae583f9065e50727b874
guilherme.agostinetti <guilherme.c.27@hotmail.com>
2023-09-28 22:35:05 -0300

    c

--- Optimizacao/Modelo_prob_mistura/prob_mistura.py
+++ Optimizacao/Modelo_prob_mistura/prob_mistura.py
@@ -1,15 +1,16 @@
 import pulp 
-import pandas as pd
+import xlrd
 import numpy as np
 
+##restricoes = pd.read_csv('funcao_objetiva.csv')
+##model = pulp.LpProblem('Exemplo_01', sense = pulp.LpMaximize)
+##print(restricoes[0])
+wb = xlrd.open_workbook('funcao_objetiva.xlsx')
+p1 =wb.sheet_by_name('instancias01')
 
-restricoes = pd.read_csv('restricoes.csv')
-
-print(restricoes)
-
-model = pulp.LpProblem('Exemplo_01', sense = pulp.LpMaximize)
-
-x = pulp.LpVariable.dicts(indexs=[1,2], cat = pulp.LpContinuous, lowBound=0,name='x')
+v = p1.rol_values(0)
+print(v)
+x = pulp.LpVariable.dicts(indexs=v, cat = pulp.LpContinuous, lowBound=0,name='x')
 
 model.addConstraint(2*x[1] + 4*x[2] <= 10, name ='restricao_1')
 model.addConstraint(6*x[1] + x[2] <= 20, name ='restricao_2')
@@ -22,5 +23,3 @@ model.solve()
 
 x_sol = {i: x[i].value() for i in [1,2]}
 print(f'x = {x_sol}')
-
-print(restricoes)
