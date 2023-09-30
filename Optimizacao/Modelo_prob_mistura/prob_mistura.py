256c0d5ac882a5f023234a7d29d026d0e30fb9cd
guilherme.agostinetti <guilherme.c.27@hotmail.com>
2023-09-30 15:07:42 -0300

    a

--- Optimizacao/Modelo_prob_mistura/prob_mistura.py
+++ Optimizacao/Modelo_prob_mistura/prob_mistura.py
@@ -1,33 +1,68 @@
 import pulp 
 import xlrd
+import numpy as np
 
+#@title = Modelo Problema da Mistura
+#@autor = Guilherme Cardoso Agostinetti
+
+# Variáveis
+# n: Número de ingredientes na mistura
+# m: Número de Componentes
+# i = 1...m
+# j = 1...n
+# a[i][j]: Fração do componente i no ingrediente j
+# b[i]: Fração do componente i na mistura
+# c[j]: Custo de uma unidade do ingrediente i
+# Q: Quantidade de Mistura à ser produzida
+
+arq = xlrd.open_workbook('C:\\Users\\Guilherme Cardoso A\\Documents\\GitHub\\Metodos_Numericos\\Optimizacao\\Modelo_prob_mistura\\instancias01.xlsx')
+plan = arq.sheet_by_name('instancia01')
+
+# C - custo de uma unidade do ingredite i
+for k in range(plan.nrows): #itere sobre os itens da aba 
+    print(plan.row(k))
+
+# vetor Custo 
+c = plan.row_values(-1)
+c = list(filter(bool, c))
+c = list(filter(lambda x: not isinstance(x, str), c))
+
+
+m = len(plan.col_values(0))-2
+# Vetor a[i][j]
+a=[0]*m
+for i in range(m):
+    a[i] = plan.row_values(i+1)
+    a[i] = list(filter(lambda x: not isinstance(x, str), a[i]))
+y = len(a[0])-len(c)
+print(y)
+print(a)
+n = len(c)
+
+
+
+'''
+#----------------------Gerando Modelo---------------------------------------------------
+# Criando Problema
+modelo = pulp.LpProblem('Exemplo_01', sense = pulp.LpMinimize)
+nn = list(range(1,n+1)) #Vetor Index
+x = pulp.LpVariable.dicts(indexs=nn , cat = pulp.LpContinuous, lowBound=0, name='x')
+
+#Gera as Restrições
+modelo.addConstraint(0.2*x[1] + 0.5*x[2] + 0.4*x[3] >= 0.3)
+modelo.addConstraint(0.6*x[1] + 0.4*x[2] + 0.4*x[3] >= 0.5)
+modelo.addConstraint(x[1] + x[2] + x[3] == 1)
+
+#Gera a Função Objetiva
 fo = 0
-##restricoes = pd.read_csv('//funcaoobjetiva.csv')
-arq = xlrd.open_workbook('C:\\Users\\Guilherme Cardoso A\\Documents\\GitHub\\Metodos_Numericos\\dados.xlsx')
-plan = arq.sheet_by_name('instancias01')
+for i in range(n):
+    fo = fo + c[i]*x[i+1]
+    print(i)
+print(fo)
 
-funcao_objetiva = plan.col_values(0)
-print(len(funcao_objetiva))
+modelo.setObjective(fo)
+modelo.solve()
 
-n_funcao_objetiva = np.arange(1,len(funcao_objetiva)+1)
-
-model = pulp.LpProblem('Exemplo_01', sense = pulp.LpMaximize)
-model
-x = pulp.LpVariable.dicts(indexs=n_funcao_objetiva, cat = pulp.LpContinuous, lowBound=0, name='x')
-print(x[1])
-teste = 2*x[1] + 4*x[2] <= 10
-model.addConstraint(teste, name ='restricao_1')
-model.addConstraint(6*x[1] + x[2] <= 20, name ='restricao_2')
-model.addConstraint(x[1] - x[2]  <= 30, name ='restricao_3')
-
-
-for i in range(1,3):
-    fo = fo + funcao_objetiva[i-1]*x[i]
-
-model.setObjective(fo)
-
-
-model.solve()
-
-x_sol = {i: x[i].value() for i in n_funcao_objetiva}
+x_sol = {i: x[i].value() for i in nn}
 print(f'x = {x_sol}')
+'''
